28260abe6185cea51ddc802af25424102ef98ff0
t2 <rocococococo92@gmail.com>
2019-07-31 18:43:58 -0500

    Ozaru

--- js/estilos.js
+++ js/estilos.js
@@ -1,19 +1,7 @@
-var working = false;
-$('.login').on('submit', function(e) {
-  e.preventDefault();
-  if (working) return;
-  working = true;
-  var $this = $(this),
-    $state = $this.find('button > .state');
-  $this.addClass('loading');
-  $state.html('Authenticating');
-  setTimeout(function() {
-    $this.addClass('ok');
-    $state.html('Welcome back!');
-    setTimeout(function() {
-      $state.html('Log in');
-      $this.removeClass('ok loading');
-      working = false;
-    }, 4000);
-  }, 3000);
-});
+function cambiar (){
+  document.getElementById("saludar").innerHTML = "Es un gusto tenerte de Vuelta";
+
+}
+document.getElementById("working").onclick = function() {
+  cambiar();
+}
